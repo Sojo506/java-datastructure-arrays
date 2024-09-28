199906ba54918d30da1255b32e85dc0ec55dfa94
Sojo506 <fsojodev@gmail.com>
2024-09-28 11:55:09 -0600

    test models

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
+import models.Student;
+import models.Vector;
+
 /* This project is about a University,
  *  where we are going to storage, and get
  *  data of students */
+
 public class Main {
     public static void main(String[] args) {
+        Student s1 = new Student("Moe");
+        Student s2 = new Student("Miller");
+        Student s3 = new Student("Cassandra");
+
+        Vector list = new Vector();
+
+        list.add(s1);
+        list.add(s2);
+
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println(list.exists(s3));
+
+        Student x = list.get(0);
+        System.out.println(x);
+
+        list.insert(1, s3);
+        System.out.println(list);
+
+        list.remove(1);
+        System.out.println(list);
 
     }
 }
