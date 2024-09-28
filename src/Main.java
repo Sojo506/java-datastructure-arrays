98f27cd0f097075b5cfe07146644110a6b5f3f55
Sojo506 <fsojodev@gmail.com>
2024-09-28 10:30:55 -0600

    init commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+/* This project is about a University,
+ *  where we are going to storage, and get
+ *  data of students */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
